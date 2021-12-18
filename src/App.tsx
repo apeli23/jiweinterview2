575a4b7a72062acaf4b6230a2fa0b5c28b5536ae
Brian Apeli <apelibrian@gmail.com>
2021-12-18 21:02:15 +0300

    fetch-data-from-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect } from 'react';
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const[data, setData] = useState(undefined)
+
+  const fetchData = async () => {
+    const result = await fetch('https://api.covid19api.com/summary')
+    const data = result.json()
+    console.log('data :>> ', data);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div><>This  is hotpink!</></div>
+  )
+};
 
-export default App;
+export default App
---
+++ src/utils/styles.tsx
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  title:{
+    textAlign: 'center',
+  },
+});
+
+export default useStyles
