6478587aca5db0973d4518ecf613fd8ebd052ebc
Brian Apeli <apelibrian@gmail.com>
2021-12-18 21:20:36 +0300

    create-types-for-server-response

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import {useState, useEffect } from 'react';
 
-const App: React.FunctionComponent = () => {
-  const[data, setData] = useState(undefined)
+type Country = {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: unknown
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
 
+type GlobalData = {
+  Date:  string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+type ResponseData = {
+  Countries: Country[];
+  Date: string;
+  Global: GlobalData;
+  ID: string;
+  Message: string;
+}
+const App: React.FunctionComponent = () => {
+  const[data, setData] = useState<ResponseData | undefined>(undefined);
+  
   const fetchData = async () => {
     const result = await fetch('https://api.covid19api.com/summary')
     const data = result.json()
